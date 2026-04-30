9724e1a180f634a2f68ac33efaf24cd5cc9da4ad
Carlos <lawlercarlospatricio@gmail.com>
2026-04-30 14:26:47 -0300

    Clase13

--- Clase13/Clase13.sql
+++ Clase13/Clase13.sql
@@ -1,6 +1,9 @@
 
 -- Laboratorio
 -- Crear la base de datos laboratorio_06, si ya existe borrarla.
+drop database if exists laboratorio_06;
+create database laboratorio_06;
+use laboratorio_06;
 
 -- Crear la siguiente tabla.
 create table cursos(
@@ -31,10 +34,17 @@ insert into cursos values
 
 -- Según la tabla cursos
 -- 1 - Sumar en uno la cantidad de alumnos inscriptos del curso Java de los días Lunes
+update cursos set inscriptos=inscriptos+1 where nombre='Java' and dias='lunes';
 -- 2 - Poner en 0 la cantidad de alumnos inscriptos de los cursos de los días Martes
+update cursos set inscriptos=0 where dias='martes';
 -- 3 - Borrar los cursos de Java en día Martes
+delete from cursos where dias='martes';
 -- 4 - Sumar 5 inscriptos en los cursos que tengan menos de 5 alumnos inscriptos.
+update cursos set inscriptos=inscriptos+5 where inscriptos=0;
 -- 5 - Cambiar el nombre de los cursos Java por Java 2 SE.
+update cursos set nombre='Java 2 SE' where nombre='Java';
+
+select * from cursos;
 
 -- Ejercicio 2-- Crear la siguiente tabla.
 
@@ -59,16 +69,141 @@ insert into empleados (nombre,apellido,seccion,sueldo) values
 	('sandra','lazante','produccion',45000),
 	('susana','mendez','administracion',86000);
 
--- 1 Cambiar al empleado Mario Lopezs de la sección administración a producción.
+-- 1 Cambiar al empleado Mario Lopez de la sección administración a producción.
+update empleados set seccion='producción' where nombre='mario' and apellido='lopez';
+
 -- 2 Aplicar un aumento de sueldo básico del 15% a los empleados de ventas.
+update empleados set sueldo=round(sueldo*1.15,2) where seccion='ventas';
+
 -- 3 Aplicar un aumento del 8% a todos los empleados de producción que tengan un
 -- 		sueldo básico menor a 6000 pesos.
+update empleados set sueldo=round(sueldo*1.08,2) 
+	where seccion='produccion' and sueldo<60000;
 -- 4 Dar de baja al empleado Susana Méndez.
+delete from empleados where nombre='susana' and apellido='mendez';
 -- 5 Aplicar un aumento de sueldo del 4% a todos los empleados que tengan un 
 -- 		básico mayor o igual a 5000 pesos.
+update empleados set sueldo=round(sueldo*1.04,2) where sueldo>=50000;
 -- 6 Aplicar un aumento de sueldo del 8% a todos los empleados que tengan un 
 -- 		básico menor a 5000 pesos.
--- 7 Agregar el campo fecha de nacimiento (date) en la tabla empleados despues del campo apellido.
+update empleados set sueldo=round(sueldo*1.08,2) where sueldo<50000;
+-- 7 Agregar el campo fecha de nacimiento (date) en la tabla empleados despues 
+-- del campo apellido.
+alter table empleados add fenaci date after apellido;
+
 -- 8 Completar una fecha de nacimiento para cada empleado.
+update empleados
+set fenaci = DATE_ADD(
+    '1970-01-01',
+    INTERVAL FLOOR(RAND() * DATEDIFF('2005-12-31', '1970-01-01')) DAY
+);
 -- 9 Listar todos los campos de empleados + una columna que calcule la edad de cada empleado.
+select 
+		nombre,
+		apellido,
+		fenaci 'fecha de nacimiento',
+		TIMESTAMPDIFF(YEAR, fenaci, CURDATE()) edad,
+		seccion,
+		sueldo 
+from empleados;
+
+select * from empleados;
+
+
+-- Laboratorio 07
+     
+drop database if exists articulos;
+create database articulos;
+use articulos;
+
+CREATE TABLE `articulos` (
+  `codigo` int(10) unsigned NOT NULL DEFAULT '0',
+  `descripcion` varchar(50) NOT NULL DEFAULT '',
+  `nombre` varchar(45) NOT NULL DEFAULT '',
+  `precio` float NOT NULL DEFAULT '0',
+  `stock` int(10) unsigned NOT NULL DEFAULT '0',
+  `marca` varchar(45) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `articulos` VALUES
+(1,'Televisores','TV LCD 19\" MODELO PFL3403',2199,40,'PHILIPS'),
+(2,'Televisores','TV LCD22\" MODELO LN22A450',2399,30,'SAMSUNG'),
+(3,'Televisores','TV LCD 32\" MODELO LC813H ',2999,25,'NOBLEX'),
+(4,'Televisores','TV LCD 26\" MODELO 26LG30R',2999,25,'LG'),
+(5,'Televisores','TV LCD 26\" MODELO CDH-L26S02',3099,20,'HITACHI'),
+(6,'Televisores','TV LCD 32\" MODELO PL3219 ',3399,20,'PHILCO'),
+(7,'Computadoras','CPU LE1250DVD2GB160VS',2199,15,'ADMIRAL'),
+(8,'Computadoras','CPU A64X246DV2G160VBOF ',2499,8,'ADMIRAL'),
+(9,'Computadoras','CPU E5200DVDR2GB320VB ',2699,5,'ADMIRAL'),
+(10,'Computadoras','CPU SG3308LA ',3099,5,'COMPAQ PRESARIO'),
+(11,'Impresoras','IMPRESORA T23 ',219,18,'EPSON'),
+(12,'Impresoras','IMPRESORA DJ-6940 ',349,20,'HEWLETT PACKARD'),
+(13,'Impresoras','IMPRESORA T33',399,12,'EPSON'),
+(14,'Impresoras','IMPRESORA K5400  ',499,8,'HEWLETT PACKARD'),
+(15,'Home Theater','HOME CINEMA SPH70 ',319,0,'PHILCO'),
+(16,'Home Theater','HOME CINEMA AMX115 ',419,0,'NOBLEX'),
+(17,'Home Theater','HOME CINEMA HT E 860 ',599,0,'X-VIEW'),
+(18,'Home Theater','HOME CINEMA DC-T990 ',759,5,'SANYO'),
+(19,'Home Theater','HOME CINEMA HT-1105U',829,18,'NOBLEX'),
+(20,'Home Theater','HOME CINEMA HT304SL-A2 ',949,6,'LG'),
+(21,'Home Theater','HOME CINEMA HT-Z110 ',999,22,'SAMSUNG'),
+(22,'Home Theater','HOME CINEMA HTS3011/55',1099,18,'PHILIPS'),
+(23,'Home Theater','HOME CINEMA HT503SH',1299,15,'LG'),
+(24,'Home Theater','HOME CINEMA HT503SH-AM',1299,12,'LG'),
+(25,'Home Theater','HOME CINEMA HT-IS10',1499,7,'SONY'),
+(26,'Home Theater','HOME CINEMA HTS3365/55',1599,5,'PHILIPS'),
+(27,'Radio','RADIO PORT RP 299  ',49,5,'NOBLEX'),
+(28,'Radio','RADIO DX-365',79,15,'KEN BROWN'),
+(29,'Radio','RADIO ICF-S10MK2/SCE',79,15,'SONY'),
+(30,'Radio','RADIO ICF-18  ',99,15,'SONY'),
+(31,'Radio','RADIO ICF-303/304 AM-FM ',129,8,'SONY'),
+(32,'Radio','RADIO SRF-59/SC E ',129,10,'SONY'),
+(33,'Notebooks','NOTEBOOK C2D2VB250WC14',3699,12,'ADMIRAL'),
+(34,'Notebooks','NOTEBOOK CQ40-300',2999,9,'COMPAQ PRESARIO'),
+(35,'Notebooks','NOTEBOOK DV2-1010',3999,7,'HEWLETT PACKARD'),
+(36,'Notebooks','NOTEBOOK DV4-1212',4199,9,'HEWLETT PACKARD'),
+(37,'Notebooks','NOTEBOOK DV4-1212',4199,10,'HEWLETT PACKARD'),
+(38,'Telefonos','TELEFONO DECT1221S INALAMBRICO',169,15,'PHILIPS'),
+(39,'Telefonos','TELEFONO CD1401B INALAMBRICO',169,8,'PHILIPS'),
+(40,'Telefonos','TELEFONO CD2401S INALAMBRICO',239,8,'PHILIPS'),
+(41,'Telefonos','TELEFONO CD1302S INALAMBRICO',249,8,'PHILIPS'),
+(42,'Telefonos','TELEFONO SANYO HNS-3300 ',69,10,'SANYO'),
+(43,'Lavarropas','LAVARROPA GAFA 7000 DIG PROGR 7KG',1679,7,'GAFA'),
+(44,'Lavarropas','LAVARROPA GAFA 7500 T750 7,5K ',1839,4,'GAFA'),
+(45,'Lavarropas','LAVARROPA GAFA ACQ7500 GRAFT750 7,5K ',1939,12,'GAFA'),
+(46,'Lavarropas','LAVARROPA LG WF-T1202TP 12KG C/S',2449,10,'LG'),
+(47,'Lavarropas','LAVARROPA LG WF-T1205TP 12K TITANIUM',2499,10,'LG'),
+(48,'Camaras digitales','CAMARA DIGITAL KODAK C813',549,10,'KODAK'),
+(49,'Camaras digitales','CAMARA DIGITAL VPCS-870EX ',579,15,'SANYO'),
+(50,'Camaras digitales','CAMARA DIGITAL C913',599,15,'KODAK'),
+(51,'Camaras digitales','CAMARA DIGITAL 760+KIT',649,15,'SAMSUNG'),
+(52,'Camaras digitales','CAMARA DIGITAL ES10',749,20,'SAMSUNG'),
+(53,'Camaras digitales','CAMARA DIGITAL ES15 ',799,40,'SAMSUNG'),
+(54,'Video camaras','CAMARA VIDEO DCR-DVD650',2749,15,'SONY'),
+(55,'Video camaras','CAMARA VIDEO DCR-DVD810 ',2999,10,'SONY'),
+(56,'Estufas','CALEFACTOR TB 2400 GN',459,12,'SIGMA'),
+(57,'Estufas','CALEFACTOR TB 3000 GN',699,12,'SIGMA'),
+(58,'Estufas','CALEFACTOR TB 5000 GN',1999,15,'SIGMA'),
+(59,'Aire acondicionado','AIRE ACONDICIONADOR SP TS-C096EMAO 2200FS',1999,15,'LG'),
+(60,'Aire acondicionado','AIRE ACONDICIONADOR SP TS-C096EMAO 2200FS ',1999,10,'LG'),
+(61,'Aire acondicionado','AIRE ACONDICIONADOR SP TS-C126EMAO 3000FS',2249,10,'LG'),
+(62,'Aire acondicionado','AIRE ACONDICIONADOR SP TS-C1865DLO 4500FS',3049,20,'LG'),
+(63,'Aire acondicionado','AIRE ACONDICIONADOR SP LS-C1863RMO 4500FS',3349,20,'LG'),
+(64,'Aire acondicionado','AIRE ACONDICIONADOR SP WSX09CG5R 2500WFS',1599,15,'WESTINGHOUSE');
+
+
+-- Usando la base de datos negocio tabla facturas.
+-- 1- 	Informar total facturado (suma de monto) en cada día.
+-- 2-	Informar total facturado (suma de monto) en cada mes.
+-- 3-	Informar total facturado (suma de monto) y cuantas facturas (count(*)) se realizaron en cada año.
+
+-- Usando la base de datos articulos.
+-- 1- Informar cantidad de televisores.
+-- 2- Informar cantidad de televisores segun marca.
+-- 3- Informar cual es el televisor más barato.
+-- 4- Aumentar un 15% los articulos de la marca 'LG'.
+-- 5- Bajar un 4% los aires acondicionados.
+-- 6- Listar los articulos ordenaros por marca,descripción,nombre,precio.
+-- 7- Cantidad de articulos según marca y descripción.
+-- 8- Mostrar el producto mas caro de la marca 'LG'
 
